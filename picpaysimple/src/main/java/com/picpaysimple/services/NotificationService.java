e0106fd4de4ae27a3bfdffb25b61d78620b98cdb
JoaoAntonioOliveiraB <joao.antonio2452@outlook.com>
2025-07-25 16:07:10 -0300

    feat: Refactor NotificationService

--- picpaysimple/src/main/java/com/picpaysimple/services/NotificationService.java
+++ picpaysimple/src/main/java/com/picpaysimple/services/NotificationService.java
@@ -1,7 +1,9 @@
 package com.picpaysimple.services;
 
+import com.picpaysimple.dtos.NotificationDTO;
 import com.picpaysimple.models.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,9 +14,17 @@ public class NotificationService {
     @Autowired
     private RestTemplate restTemplate;
 
-    public void sendNotification(User user, String message){
+    public void sendNotification(User user, String message) throws Exception {
         String email = user.getEmail();
+        NotificationDTO notificationDTO = new NotificationDTO(email, message);
 
-        ResponseEntity<String> = restTemplate.postForEntity("https://util.devi.tools/api/v1/notify", notificationRequest, String.class);
+        ResponseEntity<String> notificationResponse = restTemplate.postForEntity("https://util.devi.tools/api/v1/notify",
+                notificationDTO, String.class);
+
+        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)) {
+            System.out.println("Erro ao enviar notificação");
+            throw new Exception("Serviço de notificação está fora do ar");
+
+        }
     }
 }
